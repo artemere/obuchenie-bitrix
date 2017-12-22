c2594d442e48675f4acd50fa17c450d4ea8d8e4c
Artem Eremenko <artem.eremenko@gmail.com>
2017-12-23 00:49:36 +0300

    ПРЕДМЕНЮ

--- bitrix/templates/xml_catalog/header.php
+++ bitrix/templates/xml_catalog/header.php
@@ -21,6 +21,22 @@
                                 <h1><a href="index.html" id="logo">Arcana <em>by HTML5 UP</em></a></h1>
 
                                 <!-- Nav -->
+                                <?$APPLICATION->IncludeComponent(
+                                    "bitrix:menu",
+                                    "",
+                                    Array(
+                                        "ALLOW_MULTI_SELECT" => "N",
+                                        "CHILD_MENU_TYPE" => "left",
+                                        "DELAY" => "N",
+                                        "MAX_LEVEL" => "1",
+                                        "MENU_CACHE_GET_VARS" => array(""),
+                                        "MENU_CACHE_TIME" => "3600",
+                                        "MENU_CACHE_TYPE" => "N",
+                                        "MENU_CACHE_USE_GROUPS" => "Y",
+                                        "ROOT_MENU_TYPE" => "top",
+                                        "USE_EXT" => "N"
+                                    )
+                                );?>
                                 <nav id="nav">
                                     <ul>
                                         <li class="current"><a href="index.html">Home</a></li>
