38617f986d19ab470ae831dda58526064368b2cc
Artem Eremenko <artem.eremenko@gmail.com>
2017-12-22 22:50:45 +0300

    закончили хидер, индекс, футер

--- index.php
+++ index.php
@@ -53,7 +53,7 @@
             <div class="row">
                 <section class="6u 12u(narrower)">
                     <div class="box post">
-                        <a href="#" class="image left"><img src="images/pic01.jpg" alt="" /></a>
+                        <a href="#" class="image left"><img src="<?=SITE_TEMPLATE_PATH?>/images/pic01.jpg" alt="" /></a>
                         <div class="inner">
                             <h3>The First Thing</h3>
                             <p>Duis neque nisi, dapibus sed mattis et quis, nibh. Sed et dapibus nisl amet mattis, sed a rutrum accumsan sed. Suspendisse eu.</p>
@@ -62,7 +62,7 @@
                 </section>
                 <section class="6u 12u(narrower)">
                     <div class="box post">
-                        <a href="#" class="image left"><img src="images/pic02.jpg" alt="" /></a>
+                        <a href="#" class="image left"><img src="<?=SITE_TEMPLATE_PATH?>/images/pic02.jpg" alt="" /></a>
                         <div class="inner">
                             <h3>The Second Thing</h3>
                             <p>Duis neque nisi, dapibus sed mattis et quis, nibh. Sed et dapibus nisl amet mattis, sed a rutrum accumsan sed. Suspendisse eu.</p>
@@ -73,7 +73,7 @@
             <div class="row">
                 <section class="6u 12u(narrower)">
                     <div class="box post">
-                        <a href="#" class="image left"><img src="images/pic03.jpg" alt="" /></a>
+                        <a href="#" class="image left"><img src="<?=SITE_TEMPLATE_PATH?>/images/pic03.jpg" alt="" /></a>
                         <div class="inner">
                             <h3>The Third Thing</h3>
                             <p>Duis neque nisi, dapibus sed mattis et quis, nibh. Sed et dapibus nisl amet mattis, sed a rutrum accumsan sed. Suspendisse eu.</p>
@@ -82,7 +82,7 @@
                 </section>
                 <section class="6u 12u(narrower)">
                     <div class="box post">
-                        <a href="#" class="image left"><img src="images/pic04.jpg" alt="" /></a>
+                        <a href="#" class="image left"><img src="<?=SITE_TEMPLATE_PATH?>/images/pic04.jpg" alt="" /></a>
                         <div class="inner">
                             <h3>The Fourth Thing</h3>
                             <p>Duis neque nisi, dapibus sed mattis et quis, nibh. Sed et dapibus nisl amet mattis, sed a rutrum accumsan sed. Suspendisse eu.</p>
