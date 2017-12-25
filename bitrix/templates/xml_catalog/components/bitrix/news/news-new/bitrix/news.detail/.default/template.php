a4be17e9c6d325141d218d3d79a0ea43e95f56a9
Artem Eremenko <artem.eremenko@gmail.com>
2017-12-25 22:34:29 +0300

    сделал компонент внутренней новости

--- bitrix/templates/xml_catalog/components/bitrix/news/news-new/bitrix/news.detail/.default/template.php
+++ bitrix/templates/xml_catalog/components/bitrix/news/news-new/bitrix/news.detail/.default/template.php
@@ -10,38 +10,53 @@
 /** @var string $templateFolder */
 /** @var string $componentPath */
 /** @var CBitrixComponent $component */
-$this->setFrameMode(true);
 ?>
-<div class="news-detail">
-	<?if($arParams["DISPLAY_PICTURE"]!="N" && is_array($arResult["DETAIL_PICTURE"])):?>
-		<img
-			class="detail_picture"
-			border="0"
-			src="<?=$arResult["DETAIL_PICTURE"]["SRC"]?>"
-			width="<?=$arResult["DETAIL_PICTURE"]["WIDTH"]?>"
-			height="<?=$arResult["DETAIL_PICTURE"]["HEIGHT"]?>"
-			alt="<?=$arResult["DETAIL_PICTURE"]["ALT"]?>"
-			title="<?=$arResult["DETAIL_PICTURE"]["TITLE"]?>"
-			/>
-	<?endif?>
-	<?if($arParams["DISPLAY_DATE"]!="N" && $arResult["DISPLAY_ACTIVE_FROM"]):?>
-		<span class="news-date-time"><?=$arResult["DISPLAY_ACTIVE_FROM"]?></span>
-	<?endif;?>
-	<?if($arParams["DISPLAY_NAME"]!="N" && $arResult["NAME"]):?>
-		<h3><?=$arResult["NAME"]?></h3>
-	<?endif;?>
+
+
+<article>
+    <header>
+        <?if($arParams["DISPLAY_NAME"]!="N" && $arResult["NAME"]):?>
+                <h2><?=$arResult["NAME"]?></h2>
+            <?endif;?>
+        <?if($arParams["DISPLAY_DATE"]!="N" && $arResult["DISPLAY_ACTIVE_FROM"]):?>
+            <p><?=$arResult["DISPLAY_ACTIVE_FROM"]?></p>
+        <?endif;?>
+    </header>
+
+
+    <?if($arParams["DISPLAY_PICTURE"]!="N" && is_array($arResult["DETAIL_PICTURE"])):?>
+    <span class="image featured"><img
+                src="<?=$arResult["DETAIL_PICTURE"]["SRC"]?>"
+                width="<?=$arResult["DETAIL_PICTURE"]["WIDTH"]?>"
+                height="<?=$arResult["DETAIL_PICTURE"]["HEIGHT"]?>"
+                alt="<?=$arResult["DETAIL_PICTURE"]["ALT"]?>"
+                title="<?=$arResult["DETAIL_PICTURE"]["TITLE"]?>"
+        />
+    <?endif?>
+
+        <?if($arResult["NAV_RESULT"]):?>
+            <?if($arParams["DISPLAY_TOP_PAGER"]):?><?=$arResult["NAV_STRING"]?><br /><?endif;?>
+        <p><?echo $arResult["NAV_TEXT"];?></p>
+		<?if($arParams["DISPLAY_BOTTOM_PAGER"]):?><br /><?=$arResult["NAV_STRING"]?><?endif;?>
+        <?elseif(strlen($arResult["DETAIL_TEXT"])>0):?>
+             <p><?echo $arResult["DETAIL_TEXT"];?></p>
+        <?else:?>
+             <p><?echo $arResult["PREVIEW_TEXT"];?></p>
+        <?endif?>
+
+
+
+</article>
+
+
+<?/*<div class="news-detail">
+
+
+
 	<?if($arParams["DISPLAY_PREVIEW_TEXT"]!="N" && $arResult["FIELDS"]["PREVIEW_TEXT"]):?>
 		<p><?=$arResult["FIELDS"]["PREVIEW_TEXT"];unset($arResult["FIELDS"]["PREVIEW_TEXT"]);?></p>
 	<?endif;?>
-	<?if($arResult["NAV_RESULT"]):?>
-		<?if($arParams["DISPLAY_TOP_PAGER"]):?><?=$arResult["NAV_STRING"]?><br /><?endif;?>
-		<?echo $arResult["NAV_TEXT"];?>
-		<?if($arParams["DISPLAY_BOTTOM_PAGER"]):?><br /><?=$arResult["NAV_STRING"]?><?endif;?>
-	<?elseif(strlen($arResult["DETAIL_TEXT"])>0):?>
-		<?echo $arResult["DETAIL_TEXT"];?>
-	<?else:?>
-		<?echo $arResult["PREVIEW_TEXT"];?>
-	<?endif?>
+
 	<div style="clear:both"></div>
 	<br />
 	<?foreach($arResult["FIELDS"] as $code=>$value):
