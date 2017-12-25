c4af1888b3ee0823072fe99eeb19223d9cae516e
Artem Eremenko <artem.eremenko@gmail.com>
2017-12-25 22:54:02 +0300

    сделал компонент новости,лист

--- bitrix/templates/xml_catalog/components/bitrix/news/news-new/bitrix/news.list/.default/template.php
+++ bitrix/templates/xml_catalog/components/bitrix/news/news-new/bitrix/news.list/.default/template.php
@@ -1,18 +1,18 @@
 <?if(!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true)die(); ?>
 
 <?if($arParams["DISPLAY_TOP_PAGER"]):?>
-	<?=$arResult["NAV_STRING"]?><br />
+    <?=$arResult["NAV_STRING"]?><br />
 <?endif;?>
 
 <?foreach($arResult["ITEMS"] as $arItem):?>
-
-	<?
-	$this->AddEditAction($arItem['ID'], $arItem['EDIT_LINK'], CIBlock::GetArrayByID($arItem["IBLOCK_ID"], "ELEMENT_EDIT"));
-	$this->AddDeleteAction($arItem['ID'], $arItem['DELETE_LINK'], CIBlock::GetArrayByID($arItem["IBLOCK_ID"], "ELEMENT_DELETE"), array("CONFIRM" => GetMessage('CT_BNL_ELEMENT_DELETE_CONFIRM')));
-	?>
+    <?
+    $this->AddEditAction($arItem['ID'], $arItem['EDIT_LINK'], CIBlock::GetArrayByID($arItem["IBLOCK_ID"], "ELEMENT_EDIT"));
+    $this->AddDeleteAction($arItem['ID'], $arItem['DELETE_LINK'], CIBlock::GetArrayByID($arItem["IBLOCK_ID"], "ELEMENT_DELETE"), array("CONFIRM" => GetMessage('CT_BNL_ELEMENT_DELETE_CONFIRM')));
+    ?>
 
     <article id="<?=$this->GetEditAreaId($arItem['ID']);?>">
         <header>
+
             <?if($arParams["DISPLAY_NAME"]!="N" && $arItem["NAME"]):?>
                 <?if(!$arParams["HIDE_LINK_WHEN_NO_DETAIL"] || ($arItem["DETAIL_TEXT"] && $arResult["USER_HAVE_ACCESS"])):?>
                     <h2><a href="<?echo $arItem["DETAIL_PAGE_URL"]?>"><?echo $arItem["NAME"]?></a></h2>
@@ -20,25 +20,27 @@
                     <h2><?echo $arItem["NAME"]?></h2>
                 <?endif;?>
             <?endif;?>
+
+
             <?if($arParams["DISPLAY_DATE"]!="N" && $arItem["DISPLAY_ACTIVE_FROM"]):?>
                 <p><?echo $arItem["DISPLAY_ACTIVE_FROM"]?></p>
             <?endif?>
+
         </header>
+
+
+
         <?if($arParams["DISPLAY_PICTURE"]!="N" && is_array($arItem["PREVIEW_PICTURE"])):?>
             <?if(!$arParams["HIDE_LINK_WHEN_NO_DETAIL"] || ($arItem["DETAIL_TEXT"] && $arResult["USER_HAVE_ACCESS"])):?>
-                <a href="<?=$arItem["DETAIL_PAGE_URL"]?>"><span class="image featured"><img
-                                class="preview_picture"
-                                border="0"
-                                src="<?=$arItem["PREVIEW_PICTURE"]["SRC"]?>"
-                                width="<?=$arItem["PREVIEW_PICTURE"]["WIDTH"]?>"
-                                height="<?=$arItem["PREVIEW_PICTURE"]["HEIGHT"]?>"
-                                alt="<?=$arItem["PREVIEW_PICTURE"]["ALT"]?>"
-                                title="<?=$arItem["PREVIEW_PICTURE"]["TITLE"]?>"
-                        /></span></a>
+                <span class="image featured"><img
+                            src="<?=$arItem["PREVIEW_PICTURE"]["SRC"]?>"
+                            width="<?=$arItem["PREVIEW_PICTURE"]["WIDTH"]?>"
+                            height="<?=$arItem["PREVIEW_PICTURE"]["HEIGHT"]?>"
+                            alt="<?=$arItem["PREVIEW_PICTURE"]["ALT"]?>"
+                            title="<?=$arItem["PREVIEW_PICTURE"]["TITLE"]?>"
+                    /></span>
             <?else:?>
                 <span class="image featured"><img
-                            class="preview_picture"
-                            border="0"
                             src="<?=$arItem["PREVIEW_PICTURE"]["SRC"]?>"
                             width="<?=$arItem["PREVIEW_PICTURE"]["WIDTH"]?>"
                             height="<?=$arItem["PREVIEW_PICTURE"]["HEIGHT"]?>"
@@ -52,9 +54,10 @@
         <?endif;?>
     </article>
 <?endforeach;?>
-    <?if($arParams["DISPLAY_BOTTOM_PAGER"]):?>
+<?if($arParams["DISPLAY_BOTTOM_PAGER"]):?>
     <br /><?=$arResult["NAV_STRING"]?>
 <?endif;?>
+
 
 <?/* остатки кода из хтмл
 
